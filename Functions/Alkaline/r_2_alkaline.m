27649dd6e980d9caf9692d001b47bc1d05dc7754
Pålly <pekarste@stud.ntnu.no>
2023-05-12 16:17:46 +0200

    Misc
    Added an Alkaline version of the functions used in the first model, the olde functions were then moved to the Acidic folder

---
+++ Functions/Alkaline/r_2_alkaline.m
@@ -0,0 +1,22 @@
+function r_2_expression = r_2_alkaline(curve,E, a_OH, T)
+% r_2 takes in the coefficient and potential and returns the expression for
+% the rate of the OER reaction 
+
+%% Define Physical Constants
+R = 8.31446261815324;                                                       % J mol^-1 K^-1
+F = 96485.3329;                                                             % A s mol^-1
+n = 2;                                                                      % [-]
+E_n = 1.229;                                                                % V
+a_H2O = 1;                                                                  % [-]
+a_O2 = 0.21;                                                                % [-]
+gamma = 8.16*10^(-6);                                                       % mol/m^2 [concentration of active sites]
+
+E_rev = E_n - ((R*T)/(n*F))*log((a_OH^2)/(a_H2O*sqrt(a_O2)));               % Reversible potential - with activity of O2
+%% Expressing the rate of step 2
+k_2_0_plus = curve.k_2_0_plus;                                              % Extracting k_2_0_plus from the separate fit
+k_1_0 = curve.k_1_0;                                                        % Extracting k_1_0 from the separate fit
+alpha = curve.alpha;                                                        % Extracting talpha from the separate fit
+
+r_2_expression = gamma*k_2_0_plus*exp((1-alpha)*F*(E - E_rev)/(R*T))*a_OH./...
+    (1 + (k_1_0/a_OH)*exp(-F*(E - E_rev)/(R*T)));
+end
---
+++ Functions/Alkaline/r_2_fit_alkaline.m
@@ -0,0 +1,60 @@
+function [curve,gof] = r_2_fit_alkaline(E_data,i_data, a_OH, T, data_type)
+%r_2_fit_alkaline Fiting the expressionn for r_2_alkaline to numerical data
+%   r_2_fit_alkaline takes in potential and current density and uses it to produce
+%   coefficient for the expression of current density derived from kinetics
+%   where the second step is rds
+
+%% Define Physical Constants
+R = 8.31446261815324;                                                       % J mol^-1 K^-1
+F = 96485.3329;                                                             % A s mol^-1
+n = 2;                                                                      % Number of electrons transferred
+E_n = 1.229;                                                                 % V - If it is set to 0.4 the log curve won't fit...?
+a_H2O = 1;                                                                  % [-] - activity of water
+a_O2 = 0.21;                                                                % [-] - activity of oxygen
+gamma = 8.16*10^(-6);                                                       % mol/m^2 [concentration of active sites]
+
+E_rev = E_n - ((R*T)/(n*F))*log((a_OH^2)/(a_H2O*sqrt(a_O2)));               % Reversible potential - with activity of O2
+
+
+% Expression that is to be fitted is:
+% i = 2*F*r_2 = 2*F*(gamma*k_20p*exp((1-alpha)*F*(E-E_n)/(R*T)))*a_OH/(1 + (k_1_0/a_OH)*exp((-1)*F*(E-E_n)/(R*T)));
+% log(i) = log(2*F*gamma_k_2_0_plus*a_OH) + (1-alpha)*F*(E-E_n)/(R*T)*log(e) - log(1 +
+% (k_1_0/a_OH)*exp((-1)*F*(E-E_n)/(R*T)))
+
+%% If the data is linear then do this:
+
+if data_type == "Linear"
+    % Data
+    E = E_data;                                                             % [V] - Potential vs SHE/RHE
+    i = i_data;                                                             % [A/m^2] - Current density
+    
+    fun = @(k_2_0_plus, alpha, k_1_0, x) n*F*gamma*k_2_0_plus*exp((1-alpha)*F*(x - E_rev)/(R*T))*a_OH./(1 + (k_1_0/a_OH)*exp(-F*(x - E_rev)/(R*T)));
+    FT = fittype(fun, 'independent',{'x'}, 'coefficients',{'k_2_0_plus', 'alpha', 'k_1_0'});
+
+    FO = fitoptions('Method','NonlinearLeastSquares',...
+               'Lower',[eps, eps, eps],...                                  % k_2_0_plus alpha k_1_0
+               'Upper',[10^(8), 2, 10^(8)],...                              % k_2_0_plus alpha k_1_0
+               'StartPoint',[5.182*10^(-2), 0.5206, 2.734*10^(4)]);         % Starting point for the coefficients
+
+
+    [curve, gof] = fit(E,i,FT,FO);                                          % Curve contains the coefficients and gof some statistical data 
+    
+%% If data_type is logaritmic, then do this:
+
+elseif data_type == "Logarithmic"
+    % Data
+    E = E_data;                                                             % [V] - Potential vs SHE/RHE
+    log_i = log10(i_data);                                                  % [log(A/m^2)] - Current density
+
+    fun = @(k_2_0_plus,alpha, k_1_0,x) log10(n*F*gamma*k_2_0_plus*a_OH) + (1-alpha)*F*(x - E_rev)*log10(exp(1))/(R*T) - log10((1 + (k_1_0/a_OH)*exp(-F*(x - E_rev)/(R*T))));
+    FT = fittype(fun, 'independent',{'x'}, 'coefficients',{'k_2_0_plus', 'alpha', 'k_1_0'});
+
+    FO = fitoptions('Method','NonlinearLeastSquares',...
+               'Lower',[eps, eps, eps],...                                  % k_2_0_plus alpha k_1_0
+               'Upper',[10^2, 2, 10^(8)],...                                % k_2_0_plus alpha k_1_0
+               'StartPoint',[5.182*10^(-2), 0.5206, 2.734*10^(4)]);         % Starting point for the coefficients
+
+
+    [curve, gof] = fit(E,log_i,FT,FO);                                      % Curve contains the coefficients and gof some statistical data 
+    
+end
