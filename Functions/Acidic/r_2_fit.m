38473deb837d0ca60b7820aff05d2eebf473a3d0
Pålly <pekarste@stud.ntnu.no>
2023-05-26 19:49:01 +0200

    Changed E_n
    Changed how E_n is calculated

--- Functions/Acidic/r_2_fit.m
+++ Functions/Acidic/r_2_fit.m
@@ -8,7 +8,9 @@ function [curve, gof] = r_2_fit(E_data,i_data, a_H_plus, T, data_type)
 R = 8.31446261815324;                                                       % J mol^-1 K^-1
 F = 96485.3329;                                                             % A s mol^-1
 n = 2;                                                                      % Number of electrons transferred
-E_n = 1.229;                                                                % V
+E_OER_SHE = 1.229;                                                          % Standard reduction potential for OER vs SHE - acidic
+E_REF_RHE = 0.0;                                                            % Standard redcution potential for HER vs SHE - acidic
+E_n = E_OER_SHE - E_REF_RHE;                                                % Standard reduction potential for OER vs RHE
 a_H2O = 1;                                                                  % [-] - activity of water
 a_O2 = 0.21;                                                                % [-] - activity of oxygen
 gamma = 8.16*10^(-6);                                                       % mol/m^2 [concentration of active sites]
@@ -28,7 +30,7 @@ if data_type == "Linear"
     E = E_data;                                                             % [V] - Potential vs SHE/RHE
     i = i_data;                                                             % [A/m^2] - Current density
     
-    fun = @(k_2_0_plus, alpha, k_1_0, x) 2*F*gamma*k_2_0_plus*exp((1-alpha)*F*(x - E_rev)/(R*T))./(1 + (a_H_plus/a_H2O)*k_1_0*exp(-F*(x - E_rev)/(R*T)));
+    fun = @(k_2_0_plus, alpha, k_1_0, x) n*F*gamma*k_2_0_plus*exp((1-alpha)*F*(x - E_rev)/(R*T))./(1 + (a_H_plus/a_H2O)*k_1_0*exp(-F*(x - E_rev)/(R*T)));
     FT = fittype(fun, 'independent',{'x'}, 'coefficients',{'k_2_0_plus', 'alpha', 'k_1_0'});
 
     FO = fitoptions('Method','NonlinearLeastSquares',...
@@ -46,7 +48,7 @@ elseif data_type == "Logarithmic"
     E = E_data;                                                             % [V] - Potential vs SHE/RHE
     log_i = log10(i_data);                                                  % [log(A/m^2)] - Current density
 
-    fun = @(k_2_0_plus,alpha, k_1_0,x) log10(2*F*gamma*k_2_0_plus) + (1-alpha)*F*(x - E_rev)*log10(exp(1))/(R*T) - log10((1 + (a_H_plus/a_H2O)*k_1_0*exp(-F*(x - E_rev)/(R*T))));
+    fun = @(k_2_0_plus,alpha, k_1_0,x) log10(n*F*gamma*k_2_0_plus) + (1-alpha)*F*(x - E_rev)*log10(exp(1))/(R*T) - log10((1 + (a_H_plus/a_H2O)*k_1_0*exp(-F*(x - E_rev)/(R*T))));
     FT = fittype(fun, 'independent',{'x'}, 'coefficients',{'k_2_0_plus', 'alpha', 'k_1_0'});
 
     FO = fitoptions('Method','NonlinearLeastSquares',...
