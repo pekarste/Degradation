c1316ab23d1d790e8e68a64cab65f2042fb132fa
Pålly <pekarste@stud.ntnu.no>
2023-05-31 19:44:46 +0200

    Helping functions
    Created these helping functions for the numerical fitting procedure. They work inside numerical fitting! Hype

---
+++ Polarisation_curve_simulation_functions/k_1_plus.m
@@ -0,0 +1,18 @@
+function k_1_plus = k_1_plus(k_1_0_plus, alpha, a_OH, T, E)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+%% %%%%%%%%%%%%%%%% Defining constants %%%%%%%%%%%%%%%%%%%%%%%%% %%
+R = 8.31446261815324;                                                       % J mol^-1 K^-1
+F = 96485.3329;                                                             % A s mol^-1
+n = 2;                                                                      % [-] - number of electrons transferred in total
+E_n = 1.229;                                                                % V
+a_H2O = 1;                                                                  % [-]
+a_O2 = 0.21;                                                                % [-] - activity of oxygen
+
+%% %%%%%%%%%%% Calculating the reversible potential %%%%%%%%%%%%%%%% %%
+E_rev = E_n - ((R*T)/(n*F))*log((a_OH^2)/(a_H2O*sqrt(a_O2)));               % Reversible potential - with activity of O2
+
+k_1_plus = k_1_0_plus.*exp((1 - alpha).*F.*(E - E_rev)./(R.*T));
+
+end
