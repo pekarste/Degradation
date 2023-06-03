e7054b88850f534a1c7a569655e6bfdc3bde6f38
Pålly <pekarste@stud.ntnu.no>
2023-06-03 11:56:33 +0200

    Update to diff_equation_solver_alkaline
    Included an AbsTol which gave me smoother curves for the solutions obtyained by combining theta and gamma to gamma_theta

--- Functions/Alkaline/diff_equation_solver_alkaline.m
+++ Functions/Alkaline/diff_equation_solver_alkaline.m
@@ -1,4 +1,4 @@
-function [t, theta] = diff_equation_solver_alkaline(time_array,data_type, curve, a_OH, Temperature, k_4_0_plus, theta_0, frac)
+function [t, gamma_theta] = diff_equation_solver_alkaline(time_array,data_type, curve, a_OH, Temperature, k_4_0_plus, theta_0)
 %diff_equation_solver solves the differential equation describing the rate of change of site coverage
    
 
@@ -6,10 +6,10 @@ function [t, theta] = diff_equation_solver_alkaline(time_array,data_type, curve,
 %% Solving the differential equation
 
 t_span = [time_array(1) time_array(end)];                                   % The start en end time of the integration fot the solver
-
-[t,theta] = ode15s(@(t,theta)...                                            % using ode15s to solve the diff equation described in diff_equation(...)
-    diff_equation_alkaline(t,data_type, theta,curve,...
-    a_OH, Temperature, k_4_0_plus, frac), t_span, theta_0);                       % This would then solve the differential equation based on a value for k_4_0_plus and gamma_theta_0
+opts = odeset('RelTol',1e-10, 'AbsTol',1e-10);
+[t,gamma_theta] = ode15s(@(t,gamma_theta)...                                            % using ode15s to solve the diff equation described in diff_equation(...)
+    diff_equation_alkaline(t,data_type, gamma_theta,curve,...
+    a_OH, Temperature, k_4_0_plus), t_span, theta_0,opts);                       % This would then solve the differential equation based on a value for k_4_0_plus and gamma_theta_0
 
 
 end
