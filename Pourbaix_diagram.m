e3d92d74c1db6ffe9d06a2939d7dc754fd45b46b
Pålly <pekarste@stud.ntnu.no>
2023-06-05 17:45:16 +0200

    Changed Pourbaix diagram
    Changed the diagram to be correct with pIr

--- Pourbaix_diagram.m
+++ Pourbaix_diagram.m
@@ -80,10 +80,10 @@ text(0.35, 0.85, Label_3, 'HorizontalAlignment', 'center', 'Units', 'normalized'
 text(0.9, 0.8, Label_4, 'HorizontalAlignment', 'center', 'Units', 'normalized', 'FontSize', 15, 'Interpreter','latex', 'Rotation', -90);
 text(0.10, 0.225, Label_HER, 'HorizontalAlignment', 'center', 'Units', 'normalized', 'FontSize', 15, 'Interpreter','latex', 'Rotation', -12);
 text(0.10, 0.675, Label_OER, 'HorizontalAlignment', 'center', 'Units', 'normalized', 'FontSize', 15, 'Interpreter','latex', 'Rotation', -12);
-text(0.60, 0.9, Label_activities(1), 'HorizontalAlignment', 'center', 'Units', 'normalized', 'FontSize', 10, 'Interpreter','latex', 'Rotation', -90);
-text(0.67, 0.9, Label_activities(2), 'HorizontalAlignment', 'center', 'Units', 'normalized', 'FontSize', 10, 'Interpreter','latex', 'Rotation', -90);
-text(0.74, 0.9, Label_activities(3), 'HorizontalAlignment', 'center', 'Units', 'normalized', 'FontSize', 10, 'Interpreter','latex', 'Rotation', -90);
-text(0.81, 0.9, Label_activities(4), 'HorizontalAlignment', 'center', 'Units', 'normalized', 'FontSize', 10, 'Interpreter','latex', 'Rotation', -90);
+text(0.805, 0.9, Label_activities(1), 'HorizontalAlignment', 'center', 'Units', 'normalized', 'FontSize', 12, 'Interpreter','latex', 'Rotation', -90);
+text(0.735, 0.9, Label_activities(2), 'HorizontalAlignment', 'center', 'Units', 'normalized', 'FontSize', 12, 'Interpreter','latex', 'Rotation', -90);
+text(0.665, 0.9, Label_activities(3), 'HorizontalAlignment', 'center', 'Units', 'normalized', 'FontSize', 12, 'Interpreter','latex', 'Rotation', -90);
+text(0.595, 0.9, Label_activities(4), 'HorizontalAlignment', 'center', 'Units', 'normalized', 'FontSize', 12, 'Interpreter','latex', 'Rotation', -90);
 
 ax = gca;
 ax.XAxis.FontSize = 12;                                                     % Changing the tick size on the x-axis
